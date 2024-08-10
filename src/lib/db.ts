0d293cb2e21a26c28d18c5613cdcc4b647c21a89
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-08-10 20:16:10 +0530

    Project setup  and database setup

---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import "server-only"; // Ensure this is needed for your environment
+
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient;
+}
+
+export let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
